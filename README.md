1df29447670e4634a42595914d983d13520e22b8
Guillaume Genevrier <guillaume.genevrier@dubber.net>
2020-05-21 09:04:38 +1000

    added /etc/hosts auto update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![forthebadge](https://forthebadge.com/images/badges/you-didnt-ask-for-this.svg)](https://forthebadge.com)
+
 Set of tools used to make local dev on Kubernetes easier
--- create-ingress.sh
+++ create-ingress.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+kubectl config use-context portal-local
+
+# add ingress controller to local install
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/cloud/deploy.yaml
+
+# add local domains to /etc/hosts
+LINE="127.0.0.1 local.dubber.io portal-local s3.portal-local dashboard.portal-local"
+FILE='/etc/hosts'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
